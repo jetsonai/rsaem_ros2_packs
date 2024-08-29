6c8c20bdd7ca5a56effa56774fe05fd396cd0290
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-08-29 10:51:34 +0900

    Update ros_yolo_test_sub.py

--- ros_app_ws/ros_yolo_pack/ros_yolo_pack/ros_yolo_test_sub.py
+++ ros_app_ws/ros_yolo_pack/ros_yolo_pack/ros_yolo_test_sub.py
@@ -9,6 +9,22 @@ from rclpy.qos import QoSProfile
 #TODO
 
 #TODO
+from det_msgpack.msg import DetInfo
+
+class DetTopicSubscriber(Node):
+
+def __init__(self):
+        super().__init__('ros_yolo_test_sub')
+        qos_profile = QoSProfile(depth=10)
+        self.basicsrv_subscriber = self.create_subscription(
+            DetInfo, 'detinfo',
+            self.subscribe_detinfo,
+            qos_profile)
+
+    def subscribe_detinfo(self, msg):
+        self.get_logger().info(" label:{} cx:{} cy:{} bw:{} bh:{}".format(msg.label,
+                msg.center_x, msg.center_y, msg.b_width, msg.b_height))
+
 
 def main(args=None):
     rclpy.init(args=args)
